4829170056427a337e51201ae748453385afd7f0
Basha <basha@asalta.com>
2015-01-31 10:21:32 +0530

    Initial Web project is committed

---
+++ PreciousLifeApplication.Web/app/app.js
@@ -0,0 +1,50 @@
+﻿'use strict';
+
+var app = angular.module('app', ['ngRoute', 'ngAnimate', 'ngSanitize', 'mgcrea.ngStrap', 'LocalStorageModule', 'angular-loading-bar']);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "/app/views/home.html"
+    });
+
+    $routeProvider.when("/login", {
+        controller: "loginController",
+        templateUrl: "/app/views/login.html"
+    });
+
+    $routeProvider.when("/signup", {
+        controller: "signupController",
+        templateUrl: "/app/views/signup.html"
+    });
+    $routeProvider.when("/dashboard", {
+        controller: "dashboardController",
+        templateUrl: "/app/views/dashboard.html"
+    });
+    $routeProvider.when("/searchDonor", {
+        controller: "searchDonorController",
+        templateUrl: "/app/views/searchDonor.html"
+    });
+    $routeProvider.otherwise({ redirectTo: "/pagenotfound" });
+
+});
+
+var serviceBase = 'http://localhost:26264/';
+app.constant('ngAuthSettings', {
+    apiServiceBaseUri: serviceBase,
+    clientId: 'ngAuthApp'
+});
+
+app.config(function ($httpProvider) {
+    
+});
+
+app.config(function($popoverProvider) {
+    angular.extend($popoverProvider.defaults, {
+        html: true
+    });
+});
+
+app.run(['authService', function (authService) {
+
+}]);    
---
+++ PreciousLifeApplication.Web/app/controllers/searchDonorController.js
@@ -0,0 +1,55 @@
+﻿'use strict';
+app.controller('searchDonorController', ['$scope', function ($scope) {
+
+    $scope.showResults = false;
+    $scope.searchDonors = function() {
+        console.log("isnide search fn");
+        $scope.showResults = true;
+    };
+    
+    $scope.searchResultDonors = {        
+        Donors: [
+                  {
+                      Id: 1,
+                      Name: "DonorName1",
+                      Address: "31, First Street, KoperKharanae",
+                      City: "Mumbai",
+                      PinCode: 123456,
+                      DateOfBirth: "12/12/1980",
+                      ContactNumber: "123456789",
+                      AlternateNumber: "987654123",
+                      Weight: "65Kg"
+                  }, {
+                      Id: 2,
+                      Name: "DonorName2",
+                      Address: "28, Second Street, KoperKharanae",
+                      City: "Mumbai",
+                      PinCode: 123444,
+                      DateOfBirth: "10/10/1980",
+                      ContactNumber: "123456789",
+                      AlternateNumber: "987654123",
+                      Weight: "65Kg"
+                  }, {
+                      Id: 3,
+                      Name: "DonorName3",
+                      Address: "31, First Street, KoperKharanae",
+                      City: "Mumbai",
+                      PinCode: 123456,
+                      DateOfBirth: "12/12/1980",
+                      ContactNumber: "123456789",
+                      AlternateNumber: "987654123",
+                      Weight: "65Kg"
+                  }, {
+                      Id: 4,
+                      Name: "DonorName4",
+                      Address: "28, Second Street, KoperKharanae",
+                      City: "Mumbai",
+                      PinCode: 123444,
+                      DateOfBirth: "10/10/1980",
+                      ContactNumber: "123456789",
+                      AlternateNumber: "987654123",
+                      Weight: "65Kg"
+                  }
+        ]
+    };
+}]);
